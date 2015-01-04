e13a2277a2d57a79777d6d0621582b4a76894f34
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-04 20:37:28 +0100

    removed more complexity

--- init-project.sh
+++ init-project.sh
@@ -12,10 +12,6 @@ if [[ ! ${CAPITALIZED} =~ ^([A-Z][a-z0-9]+){2,}$ ]]; then
     exit 1
 fi
 
-OLD_UNDERSCORE="example_class"
-OLD_BIN="example-script"
-OLD_CAPITALIZED="ExampleClass"
-
 UNDERSCORE=$(echo ${CAPITALIZED} | sed -E 's/([A-Za-z0-9])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]')
 BIN=$(echo ${UNDERSCORE} | sed -E 's/_/-/g')
 INITIALS=$(echo ${CAPITALIZED} | sed 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]' )
@@ -27,11 +23,11 @@ echo "Initials: ${INITIALS}"
 
 exit 0
 
-SUBSITUTE_FILES="bin/${OLD_BIN} test/test_${ORIGINAL_UNDERSCORE}.py"
+SUBSITUTE_FILES="bin/example-script test/test_${ORIGINAL_UNDERSCORE}.py"
 SED_CMD="sed -i \"\" -e"
 ${SED_CMD} "s/ec/${INITIALS}/g" ${SUBSITUTE_FILES}
-${SED_CMD} "s/${OLD_UNDERSCORE}/${UNDERSCORE_CAPITALIZED}/g" ${SUBSITUTE_FILES}
-${SED_CMD} "s/${OLD_CAPITALIZED}/${CAPITALIZED}/g" ${SUBSITUTE_FILES}
+${SED_CMD} "s/example_class/${UNDERSCORE}/g" ${SUBSITUTE_FILES}
+${SED_CMD} "s/ExampleClass/${CAPITALIZED}/g" ${SUBSITUTE_FILES}
 
-#mv "test/test_${OLD_UNDERSCORE}.py" "test/test_${UNDERSCORE}.py"
-#mv "bin/${OLD_BIN}" "bin/${INITIALS}"
+#mv "test/test_example_class.py" "test/test_${UNDERSCORE}.py"
+#mv "bin/example-script" "bin/${INITIALS}"
