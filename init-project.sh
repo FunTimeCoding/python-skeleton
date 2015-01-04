0548c97cc71fb70f824f1db951dbc6a0047bc53b
Alexander Reitzel <funtimecoding@gmail.com>
2015-01-04 20:43:25 +0100

    even more cleaning up the new project script

--- init-project.sh
+++ init-project.sh
@@ -1,33 +1,31 @@
 #!/bin/sh -e
 
-CAPITALIZED=${1}
+CAMEL=${1}
 
-if [ "${CAPITALIZED}" = "" ]; then
-    echo "Usage: ${0} MyCapitalizedProjectName"
+if [ "${CAMEL}" = "" ]; then
+    echo "Usage: ${0} MyUpperCamelCaseProjectName"
     exit 1
 fi
 
-if [[ ! ${CAPITALIZED} =~ ^([A-Z][a-z0-9]+){2,}$ ]]; then
+if [[ ! ${CAMEL} =~ ^([A-Z][a-z0-9]+){2,}$ ]]; then
     echo "Project name must be in UpperCamelCase."
     exit 1
 fi
 
-UNDERSCORE=$(echo ${CAPITALIZED} | sed -E 's/([A-Za-z0-9])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]')
-BIN=$(echo ${UNDERSCORE} | sed -E 's/_/-/g')
-INITIALS=$(echo ${CAPITALIZED} | sed 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]' )
+UNDERSCORE=$(echo ${CAMEL} | sed -E 's/([A-Za-z0-9])([A-Z])/\1_\2/g' | tr '[:upper:]' '[:lower:]')
+DASH=$(echo ${UNDERSCORE} | sed -E 's/_/-/g')
+INITIALS=$(echo ${CAMEL} | sed 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]' )
 
+echo "Camel: ${CAMEL}"
 echo "Underscore: ${UNDERSCORE}"
-echo "Bin: ${BIN}"
-echo "Capitalized: ${CAPITALIZED}"
+echo "Dash: ${DASH}"
 echo "Initials: ${INITIALS}"
-
-exit 0
 
 SUBSITUTE_FILES="bin/example-script test/test_${ORIGINAL_UNDERSCORE}.py"
 SED_CMD="sed -i \"\" -e"
 ${SED_CMD} "s/ec/${INITIALS}/g" ${SUBSITUTE_FILES}
 ${SED_CMD} "s/example_class/${UNDERSCORE}/g" ${SUBSITUTE_FILES}
-${SED_CMD} "s/ExampleClass/${CAPITALIZED}/g" ${SUBSITUTE_FILES}
+${SED_CMD} "s/ExampleClass/${CAMEL}/g" ${SUBSITUTE_FILES}
 
-#mv "test/test_example_class.py" "test/test_${UNDERSCORE}.py"
-#mv "bin/example-script" "bin/${INITIALS}"
+mv "test/test_example_class.py" "test/test_${UNDERSCORE}.py"
+mv "bin/example-script" "bin/${INITIALS}"
