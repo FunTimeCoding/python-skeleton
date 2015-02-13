d3a1378d6efe8a99b75704d66424a0a5bdd46010
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-13 13:38:48 +0100

    update coveragerc to ignore setup.py, make clear-cache script also remove build directory and .coverage file

--- clear-cache.sh
+++ clear-cache.sh
@@ -3,3 +3,5 @@ echo "Deleting python cache files and directories and the .egg-info directory."
 find . -name "__pycache__" | xargs -I {} rm -rfv "{}"
 find . -name "*.egg-info" | xargs -I {} rm -rfv "{}"
 find . -name "*.pyc" | xargs -I {} rm -v "{}"
+rm -rfv build
+rm -f .coverage
