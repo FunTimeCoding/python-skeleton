04c09386416e99559daf9fd51aff8bc12cf4125e
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-26 12:44:09 +0100

    update pylint config

--- clear-cache.sh
+++ clear-cache.sh
@@ -2,6 +2,6 @@
 echo "Deleting python cache files and directories and the .egg-info directory."
 find . -name "__pycache__" | xargs -I {} rm -rfv "{}"
 find . -name "*.egg-info" | xargs -I {} rm -rfv "{}"
-find . -name "*.pyc" | xargs -I {} rm -v "{}"
+find . -name "*.pyc" | xargs -I {} rm -fv "{}"
 rm -rfv build
-rm -f .coverage
+rm -fv .coverage
