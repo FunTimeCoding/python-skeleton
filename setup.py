4f35253d03113661794a919cdea6fce07250689b
Alexander Reitzel <funtimecoding@gmail.com>
2015-02-28 19:56:23 +0100

    unify setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='python-skeleton',
-      version='0.1',
-      description='Stub description for python-skeleton.',
-      install_requires=[],
-      scripts=['bin/ps'],
-      packages=['python_skeleton'],
-      author='Alexander Reitzel',
-      author_email='funtimecoding@gmail.com',
-      url='http://example.org',
-      download_url='http://example.org/python-skeleton.tar.gz')
+setup(
+    name='python-skeleton',
+    version='0.1',
+    description='Stub description for python-skeleton.',
+    install_requires=[],
+    scripts=['bin/ps'],
+    packages=['python_skeleton'],
+    author='Alexander Reitzel',
+    author_email='funtimecoding@gmail.com',
+    url='http://example.org',
+    download_url='http://example.org/python-skeleton.tar.gz'
+)
