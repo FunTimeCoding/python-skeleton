4d8d7b81756f0c90d2a19955c5b765a7fef72826
Alexander Reitzel <funtimecoding@gmail.com>
2016-02-06 15:37:09 +0100

    unify init and sync scripts with other skeleton projects

--- init-project.sh
+++ init-project.sh
@@ -12,22 +12,22 @@ fi
 
 OPERATING_SYSTEM=$(uname)
 
-if [ "${OPERATING_SYSTEM}" = "Darwin" ]; then
-    FIND="gfind"
-    SED="gsed"
-else
-    FIND="find"
+if [ "${OPERATING_SYSTEM}" = "Linux" ]; then
     SED="sed"
+    FIND="find"
+else
+    SED="gsed"
+    FIND="gfind"
 fi
 
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
 UNDERSCORE=$(echo "${DASH}" | ${SED} -E 's/-/_/g')
+rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|\.git|\.idea|\.pyvenv|\.tox|__pycache__)/.*$' -exec sh -c '${1} -i -e "s/PythonSkeleton/${2}/g" -e "s/python-skeleton/${3}/g" -e "s/python_skeleton/${4}/g" -e "s/bin\/ps/bin\/${5}/g" ${6}' '_' "${SED}" "${CAMEL}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
 git mv bin/ps bin/"${INITIALS}"
-git mv python_skeleton/python_skeleton.py "python_skeleton/${UNDERSCORE}.py"
+git mv python_skeleton/python_skeleton.py python_skeleton/"${UNDERSCORE}".py
 git mv python_skeleton "${UNDERSCORE}"
-git mv tests/test_python_skeleton.py "tests/test_${UNDERSCORE}.py"
-rm init-project.sh sync-project.sh
+git mv tests/test_python_skeleton.py tests/test_"${UNDERSCORE}".py
 echo "Done. Files were edited and moved. Review those changes."
--- sync-project.sh
+++ sync-project.sh
@@ -43,15 +43,11 @@ cp .gitignore "${TARGET_PROJECT}"
 cp .pylintrc "${TARGET_PROJECT}"
 cp .pytest.ini "${TARGET_PROJECT}"
 cp .pytest-ci.ini "${TARGET_PROJECT}"
-rm "${TARGET_PROJECT}/init-project.sh"
-rm "${TARGET_PROJECT}/sync-project.sh"
 DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
 UNDERSCORE=$(echo "${DASH}" | ${SED} -E 's/-/_/g')
-echo "DASH: ${DASH}"
-echo "INITIALS: ${INITIALS}"
-echo "UNDERSCORE: ${UNDERSCORE}"
 cd "${TARGET_PROJECT}" || exit 1
+rm init-project.sh sync-project.sh
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|\.git|\.idea|\.pyvenv|\.tox|__pycache__)/.*$' -exec sh -c '${1} -i -e "s/PythonSkeleton/${2}/g" -e "s/python-skeleton/${3}/g" -e "s/python_skeleton/${4}/g" -e "s/bin\/ps/bin\/${5}/g" ${6}' '_' "${SED}" "${CAMEL}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
 echo "Done. Files were copied to ${TARGET_PROJECT} and modified. Review those changes."
