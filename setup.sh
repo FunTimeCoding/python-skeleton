9e1b89646ff9b02becac8bf183dde3e9cf3bf38e
Alexander Reitzel <funtimecoding@gmail.com>
2017-04-05 17:24:30 +0200

    rename development setup tscript to plain setup, add doc and dict directories for documentation and dictionaries respectively, minor refactorings, update init and sync scripts along the way

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+if [ "${1}" = --help ]; then
+    echo "Usage: ${0} [--development]"
+fi
+
+if [ "${1}" = --development ]; then
+    wget --quiet --output-document - cfg.greenshininglake.org/python3.sh | sh -e
+    wget --quiet --output-document - cfg.greenshininglake.org/shellcheck.sh | sh -e
+fi
+
+SYSTEM=$(uname)
+
+if [ "${SYSTEM}" = Linux ]; then
+    sudo apt-get --quiet 2 install libenchant-dev
+fi
+
+pip3 install --upgrade --user --requirement requirements.txt
+pip3 install --user --editable .
--- sync-project.sh
+++ sync-project.sh
@@ -1,23 +1,23 @@
 #!/bin/sh -e
 
-TARGET_PROJECT="${1}"
+TARGET="${1}"
 
-if [ "${TARGET_PROJECT}" = "" ]; then
+if [ "${TARGET}" = "" ]; then
     echo "Usage: ${0} TARGET_PROJECT"
 
     exit 1
 fi
 
-if [ ! -d "${TARGET_PROJECT}" ]; then
-    echo "Target directory ${TARGET_PROJECT} does not exist."
+if [ ! -d "${TARGET}" ]; then
+    echo "Target directory ${TARGET} does not exist."
 
     exit 1
 fi
 
-CAMEL=$(head -n1 "${TARGET_PROJECT}"/README.md | awk '{ print $2 }' | grep -E '^([A-Z]+[a-z0-9]*){2,}$') || CAMEL=""
+CAMEL=$(head -n 1 "${TARGET}"/README.md | awk '{ print $2 }' | grep --extended-regexp '^([A-Z]+[a-z0-9]*){2,}$') || CAMEL=""
 
 if [ "${CAMEL}" = "" ]; then
-    echo "Could not determine the projects name in ${TARGET_PROJECT}."
+    echo "Could not determine the projects name in ${TARGET}."
 
     exit 1
 fi
@@ -32,23 +32,25 @@ else
     SED=sed
 fi
 
-cp ./*.md "${TARGET_PROJECT}"
-cp ./*.sh "${TARGET_PROJECT}"
-cp tox.ini "${TARGET_PROJECT}"
-cp requirements.txt "${TARGET_PROJECT}"
-cp sonar-project.properties "${TARGET_PROJECT}"
-cp setup.py "${TARGET_PROJECT}"
-cp custom.dic "${TARGET_PROJECT}"
-cp .coveragerc "${TARGET_PROJECT}"
-cp .gitignore "${TARGET_PROJECT}"
-cp .pylintrc "${TARGET_PROJECT}"
-cp .pytest.ini "${TARGET_PROJECT}"
-cp .pytest-ci.ini "${TARGET_PROJECT}"
-DASH=$(echo "${CAMEL}" | ${SED} -E 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+cp ./*.md "${TARGET}"
+cp ./*.sh "${TARGET}"
+cp tox.ini "${TARGET}"
+cp requirements.txt "${TARGET}"
+cp sonar-project.properties "${TARGET}"
+cp setup.py "${TARGET}"
+cp dict/* "${TARGET}/dict"
+cp doc/* "${TARGET}/doc"
+cp .coveragerc "${TARGET}"
+cp .gitignore "${TARGET}"
+cp .pylintrc "${TARGET}"
+cp .pytest.ini "${TARGET}"
+cp .pytest-ci.ini "${TARGET}"
+DASH=$(echo "${CAMEL}" | ${SED} --regexp-extended 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
 INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
-UNDERSCORE=$(echo "${DASH}" | ${SED} -E 's/-/_/g')
-cd "${TARGET_PROJECT}" || exit 1
+UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
+cd "${TARGET}" || exit 1
 rm init-project.sh sync-project.sh
+cp dict/python-skeleton.dic "dict/${DASH}.dic"
 # shellcheck disable=SC2016
 ${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|\.git|\.idea|\.pyvenv|\.tox|__pycache__)/.*$' -exec sh -c '${1} -i -e "s/PythonSkeleton/${2}/g" -e "s/python-skeleton/${3}/g" -e "s/python_skeleton/${4}/g" -e "s/bin\/ps/bin\/${5}/g" "${6}"' '_' "${SED}" "${CAMEL}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
-echo "Done. Files were copied to ${TARGET_PROJECT} and modified. Review those changes."
+echo "Done. Files were copied to ${TARGET} and modified. Review those changes."
