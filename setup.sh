5576467cd0c00afe9ad9e725e468da31060dd3da
Alexander Reitzel <funtimecoding@gmail.com>
2017-06-10 18:44:32 +0200

    fix typo in requirements, use venv in setup.sh, allow skipping it too

--- setup.sh
+++ setup.sh
@@ -1,9 +1,31 @@
 #!/bin/sh -e
 
+if [ "${1}" = --help ]; then
+    echo "Usage: ${0} [--no-venv]"
+fi
+
+USE_VENV=true
+
+if [ "${1}" = --no-venv ]; then
+    USE_VENV=false
+fi
+
 SYSTEM=$(uname)
 
 if [ "${SYSTEM}" = Linux ]; then
-    sudo apt-get --quiet 2 install libenchant-dev
+    dpkg --list | grep --quiet 'ii  libenchant-dev' && FOUND=true || FOUND=false
+
+    if [ "${FOUND}" = false ]; then
+        sudo apt-get --quiet 2 install libenchant-dev
+    fi
+fi
+
+if [ "${USE_VENV}" = true ]; then
+    if [ ! -d .venv ]; then
+        python3 -m venv .venv
+    fi
+
+    . .venv/bin/activate
 fi
 
 pip3 install --upgrade --requirement requirements.txt
