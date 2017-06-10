d1400b6d4bda803dba75daf06a8ab1480648627b
Alexander Reitzel <funtimecoding@gmail.com>
2017-06-10 16:47:10 +0200

    update the statistics script

--- project-statistics.sh
+++ project-statistics.sh
@@ -1,6 +1,25 @@
 #!/bin/sh -e
 
-FILE_COUNT=$(find . -type f | wc -l)
-DIRECTORY_COUNT=$(find . -type d | wc -l)
+SYSTEM=$(uname)
+
+if [ "${SYSTEM}" = Darwin ]; then
+    WC=gwc
+    FIND=gfind
+else
+    WC=wc
+    FIND=find
+fi
+
+FILE_COUNT=$(${FIND} . -type f | ${WC} --lines)
 echo "FILE_COUNT: ${FILE_COUNT}"
+
+DIRECTORY_COUNT=$(${FIND} . -type d | ${WC} --lines)
 echo "DIRECTORY_COUNT: ${DIRECTORY_COUNT}"
+
+ALL_CODE=$(${FIND} . -name '*.py' | xargs cat)
+
+LINE_COUNT=$(echo "${ALL_CODE}" | ${WC} --lines)
+echo "LINE_COUNT: ${LINE_COUNT}"
+
+NON_BLANK_LINE_COUNT=$(echo "${ALL_CODE}" | grep --invert-match --regexp '^$' | ${WC} --lines)
+echo "NON_BLANK_LINE_COUNT: ${NON_BLANK_LINE_COUNT}"
