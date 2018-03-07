1673b4fbb0587cc22f2bd896dfdbb5a23e3352f9
Alexander Reitzel <funtimecoding@gmail.com>
2018-03-07 18:34:27 +0100

    synchronize the synchronize script

--- script/skeleton/synchronize.sh
+++ script/skeleton/synchronize.sh
@@ -9,15 +9,15 @@ if [ "${TARGET}" = "" ]; then
 fi
 
 if [ ! -d "${TARGET}" ]; then
-    echo "Target directory ${TARGET} does not exist."
+    echo "Target directory does not exist."
 
     exit 1
 fi
 
-CAMEL=$(head -n 1 "${TARGET}"/README.md | awk '{ print $2 }' | grep --extended-regexp '^([A-Z]+[a-z0-9]*){2,}$') || CAMEL=""
+NAME=$(head -n 1 "${TARGET}"/README.md | awk '{ print $2 }' | grep --extended-regexp '^([A-Z]+[a-z0-9]*){2,}$') || NAME=""
 
-if [ "${CAMEL}" = "" ]; then
-    echo "Could not determine project name in ${TARGET}."
+if [ "${NAME}" = "" ]; then
+    echo "Could not determine the project name."
 
     exit 1
 fi
@@ -49,11 +49,11 @@ cp .gitignore "${TARGET}"
 cp .pylintrc "${TARGET}"
 cp .pytest.ini "${TARGET}"
 cp .pytest-ci.ini "${TARGET}"
-DASH=$(echo "${CAMEL}" | ${SED} --regexp-extended 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
-INITIALS=$(echo "${CAMEL}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
-UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
 cd "${TARGET}" || exit 1
 rm -rf script/skeleton
+DASH=$(echo "${NAME}" | ${SED} --regexp-extended 's/([A-Za-z0-9])([A-Z])/\1-\2/g' | tr '[:upper:]' '[:lower:]')
+INITIALS=$(echo "${NAME}" | ${SED} 's/\([A-Z]\)[a-z]*/\1/g' | tr '[:upper:]' '[:lower:]')
+UNDERSCORE=$(echo "${DASH}" | ${SED} --regexp-extended 's/-/_/g')
 touch "documentation/dictionary/${DASH}.dic"
 # shellcheck disable=SC2016
-${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|tmp|\.git|\.idea|\.venv|\.tox|\.cache|\.vagrant|__pycache__|[a-z_]+\.egg-info)/.*$' -exec sh -c '${1} --in-place --expression "s/PythonSkeleton/${2}/g" --expression "s/python-skeleton/${3}/g" --expression "s/python_skeleton/${4}/g" --expression "s/pyskel/${5}/g" "${6}"' '_' "${SED}" "${CAMEL}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
+${FIND} . -type f -regextype posix-extended ! -regex '^.*/(build|tmp|\.git|\.idea|\.venv|\.tox|\.cache|\.vagrant|__pycache__|[a-z_]+\.egg-info)/.*$' -exec sh -c '${1} --in-place --expression "s/PythonSkeleton/${2}/g" --expression "s/python-skeleton/${3}/g" --expression "s/python_skeleton/${4}/g" --expression "s/pyskel/${5}/g" "${6}"' '_' "${SED}" "${NAME}" "${DASH}" "${UNDERSCORE}" "${INITIALS}" '{}' \;
