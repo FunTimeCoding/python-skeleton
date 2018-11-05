c1adf1e43a3d787c16f2c6b6467cda308f5c04ba
Alexander Reitzel <funtimecoding@gmail.com>
2018-11-05 18:31:39 +0100

    integrate package and publish scripts from jjm

---
+++ lib/project.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+NAME=python-skeleton
+PROJECT_VERSION=0.1.0
+EMAIL='funtimecoding@gmail.com'
+export EMAIL
+PACKAGE_VERSION=1
+COMBINED_VERSION="${PROJECT_VERSION}-${PACKAGE_VERSION}"
