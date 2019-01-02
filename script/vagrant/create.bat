ab54733fe3d199f12368be4969b3cc1fe80db557
Alexander Reitzel <funtimecoding@gmail.com>
2019-01-02 23:29:18 +0100

    integrate changes from chat-bridge

--- script/vagrant/create.bat
+++ script/vagrant/create.bat
@@ -1,11 +1,11 @@
+@echo off
 if not exist .venv python -m venv .venv
 
 if not exist tmp mkdir tmp
 
 if not exist tmp/bootstrap-salt.sh powershell -Command "Invoke-WebRequest https://bootstrap.saltstack.com -OutFile tmp/bootstrap-salt.sh"
 
-if not exist tmp/salt mkdir tmp/salt
-xcopy /s configuration/minion.yaml tmp/salt/minion.conf
+copy configuration\minion.yaml tmp\salt\minion.conf
 
 vagrant up
 pause
